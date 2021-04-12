1c36eb8373756a3b99790b58ae93e23192d93480
pastko <storyofstory@naver.com>
2021-04-12 20:11:06 +0900

    elastic search
    res : json data 변경으로 인한 수정
           - res.body 내 값으로 변경

--- elastic/els.js
+++ elastic/els.js
@@ -7,6 +7,7 @@ const client = new Client({
   }
 })
 const XLSX = require('xlsx');
+const moment = require('moment');
 
 /*
 const elastic = require('elasticsearch');
@@ -24,52 +25,53 @@ const client = new elastic.Client({
 });
 */
 
-function exportExcel(_file,_data){ 
+function exportExcel(_file, _data) {
   // step 1. workbook 생성
   const wb = XLSX.utils.book_new();
 
   // step 2. 시트 만들기 
   let newWorksheet = excelHandler.getWorksheet(_data);
-  
+
   // step 3. workbook에 새로만든 워크시트에 이름을 주고 붙인다.  
-  XLSX.utils.book_append_sheet(wb, newWorksheet, _file.replace("*",""));
+  XLSX.utils.book_append_sheet(wb, newWorksheet, _file.replace("*", ""));
 
   // step 4. 엑셀 파일 만들기 
-  XLSX.writeFile(wb, _file.replace('*','')+'.xlsx');
+  XLSX.writeFile(wb, _file.replace('*', '') + '.xlsx');
 }
 
 
-var excelHandler = {    
-  getExcelData : function(){
-      return [{'name':'삼성 갤럭시 s11' , 'price':'200000'}, {'name':'삼성 갤럭시 s11' , 'price':'200000'}]; 
+var excelHandler = {
+  getExcelData: function () {
+    return [];
   },
-  getExcelData : function(_data){
-      let rs = [];
-      _data.forEach(res => {
-          let _tmp = res['@timestamp']
-          _tmp = _tmp.replace("T"," ");
-          _tmp = _tmp.replace("Z","");
-          let _time = moment(_tmp);
-          _time.add(9,'h');            
-          rs.push({ "IDN" : res.IDN,  "Time" : _time.format("YYYY-MM-DD HH:mm:ss"), "DATA" : res.data  , "RMSG" : res.RMSG, "COMPANY" : res.COMPANY });
-      });
-      return rs;
+  getExcelData: function (_data) {
+    let rs = [];
+    _data.forEach(res => {
+      let _tmp = res['@timestamp']
+      _tmp = _tmp.replace("T", " ");
+      _tmp = _tmp.replace("Z", "");
+      let _time = moment(_tmp);
+      _time.add(9, 'h');
+      rs.push({ "IDN": res.IDN, "Time": _time.format("YYYY-MM-DD HH:mm:ss"), "DATA": res.data, "RMSG": res.RMSG, "COMPANY": res.COMPANY });
+    });
+    return rs;
   },
-  getWorksheet : function(_data){
-      return XLSX.utils.json_to_sheet(this.getExcelData(_data));
+  getWorksheet: function (_data) {
+    return XLSX.utils.json_to_sheet(this.getExcelData(_data));
   }
 }
 
 
 
-async function s_query() {
+async function s_query(_idn) {
   return new Promise((resolve, reject) => {
     var list = [];
     var count = 0;
+
     client.search({
-      index: '0000000000000100',
+      index: _idn,
       scroll: '5s',
-      size: 10,
+      size: 100,
       body: {
         query: {
           "bool": {
@@ -77,8 +79,8 @@ async function s_query() {
             "filter": [{
               "range": {
                 "@timestamp": {
-                  "gte": "2021-01-01T15:00:00.000Z",
-                  "lte": "2021-01-02T14:59:59.000Z",
+                  "gte": "2020-12-31T15:00:00.000Z",
+                  "lte": "2021-03-31T14:59:59.000Z",
                   "format": "strict_date_optional_time"
                 }
               }
@@ -96,35 +98,44 @@ async function s_query() {
         ]
       }
     }, function getMoreDone(err, res) {
-      if (err) { console.log(err); reject(err) };
-      //console.log(res.hits.total);
-      //console.log(res.body.hits);
-      count += res.body.hits.hits.length;
-
-      for (dt of res.body.hits.hits) {
-        list.push(dt['_source']);
-      }
-      console.log(list.length);
-
-      if (count != res.body.hits.total.value) {
-        client.scroll({
-          scrollId: res.body._scroll_id,
-          scroll: '5s'
-        }, getMoreDone);
-      }
+      if (err) { reject(err) };
+      if (res.statusCode !== 200) {
+        console.log(res);
+        reject('errcode : ' + res.statuscode);
+      } 
       else {
+        //console.log(res.hits.total);
+        //console.log(res.body.hits);
+
+        count += res.body.hits.hits.length;
+
+        for (dt of res.body.hits.hits) {
+          list.push(dt['_source']);
+        }
         //console.log(list.length);
-        resolve(list);
+
+        if (count != res.body.hits.total.value) {
+          client.scroll({
+            scrollId: res.body._scroll_id,
+            scroll: '5s'
+          }, getMoreDone);
+        }
+        else {
+          //console.log(list.length);
+          resolve(list);
+        }
       }
     })
   });
 }
 
 
-s_query().then((res) => {
+const idn = '0000000000000105';
+
+s_query(idn).then((res) => {
   console.log("result data count : ");
   console.log(res.length);
-}).catch((err) => {
-  if (err) console.log(err);
-});
+
+  exportExcel(("NODE" + idn), res);
+}).catch(console.log);
 
