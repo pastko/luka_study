d975d074df66d0241b7905c1e9636abdb8e03b1f
pastko <storyofstory@naver.com>
2021-04-13 20:12:40 +0900

    pastko-sping
    spring boot start
    
    error: lombok error
    java: java.lang.IllegalAccessError: class lombok.javac.apt.LombokProcessor

---
+++ pastko-spring/src/main/java/com/example/pastko/PastkoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pastko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastkoApplication.class, args);
+    }
+
+}
---
+++ pastko-spring/src/test/java/com/example/pastko/PastkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pastko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastkoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
