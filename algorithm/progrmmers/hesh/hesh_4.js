ed02324a99e428001828233aaff08614fdef7ecc
pastko <storyofstory@naver.com>
2021-04-22 22:56:19 +0900

    Update hesh_4.js
    다른 문제풀이

--- algorithm/progrmmers/hesh/hesh_4.js
+++ algorithm/progrmmers/hesh/hesh_4.js
@@ -194,3 +194,19 @@ function se (genres, plays)
 }
 
 console.log(se(['A', 'A', 'B', 'A', 'B', 'B'], [5, 5, 6, 5, 7, 7]))       //  == [4, 5, 0, 1]
+
+
+function solution(genres, plays) {
+    const count = {};
+    let answer = [];
+    const acc = genres.reduce((a, c, i) => {
+        debugger;
+        count[c] ? count[c].push([i, plays[i]]) : count[c] = [[i, plays[i]]];
+        return a.set(c, a.get(c) ? a.get(c) + plays[i] : plays[i]), a;
+    }, new Map());
+
+    [...acc].sort((a, b) => b[1] - a[1]).map(v => {
+            answer = answer.concat(count[v[0]].sort((c, d)=>d[1]-c[1]).slice(0,2));
+    });
+    return answer.map(v=>v[0]);
+}
