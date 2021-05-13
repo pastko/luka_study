bc14748c8441d5529fac5f20f0b322597faf01e4
pastko <storyofstory@naver.com>
2021-05-13 21:10:06 +0900

    Create greedy_3.js
    그리드 3번 문제

---
+++ algorithm/progrmmers/greedy/greedy_3.js
@@ -0,0 +1,88 @@
+/*****
+ * 
+ * 
+ * 문제 설명
+어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
+
+예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 이 중 가장 큰 숫자는 94 입니다.
+
+문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
+
+제한 조건
+number는 1자리 이상, 1,000,000자리 이하인 숫자입니다.
+k는 1 이상 number의 자릿수 미만인 자연수입니다.
+입출력 예
+number	k	return
+"1924"	2	"94"
+"1231234"	3	"3234"
+"4177252841"	4	"775841"
+
+ * 
+ * 
+ * 
+ */
+
+function solution(number, k) {
+   
+    number = number.split("");
+    for (let i = 0, j = 0; i < number.length ; ++i) {
+        if( j == k ){
+            console.log("###########");
+            console.log("["+i+":"+j+"]");
+            console.log(number)
+            console.log("###########");
+
+            break;
+        }
+
+
+        if(i == number.length -1 && j != k )        
+        {
+            number = number.filter(e=>e!='0');
+            console.log(number)
+            i = 0;
+        }
+
+        
+
+
+        console.log("["+j+":"+i+"]")
+        if (number[i] < number[i+1] ) {
+            number[i] = '0';
+            j++;
+        }
+        
+        console.log("------")
+        console.log(number);
+    }
+
+    return number.filter(e=>e!='0').join("");
+}
+
+
+/**
+ * 
+ * main core
+ * 
+ * 
+ */
+ let num1 = { "number" : "1924" , "k" : 2};
+ let num2 = { "number" : "1231234" , "k" : 3};
+ let num3 = { "number" : "4177252841" , "k" : 4};
+
+
+console.log(solution(num3.number,num3.k));
+
+
+
+
+
+
+/****
+ * 
+ * another problem solving
+ * 
+ * 
+ * 
+ * 
+ */
