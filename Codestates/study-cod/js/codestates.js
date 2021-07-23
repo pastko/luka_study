1c01812ce6b16448faf2940aaf3e85af76b32f3e
pastko <storyofstory@naver.com>
2021-07-23 09:27:03 +0900

    Update codestates.js

--- Codestates/study-cod/js/codestates.js
+++ Codestates/study-cod/js/codestates.js
@@ -443,3 +443,27 @@ let documents = [7, 4, 5, 6];
 
 let output = queuePrinter(bufferSize, capacities, documents);
 console.log(output) // 8
+
+
+
+const bubbleSort = function (arr) {
+    // TODO: 여기에 코드를 작성합니다.
+    let switchingFlag = false
+    for(let i = 0 ; i < arr.length ; ++ i){
+        for(let j = 0 ; j + 1 + i < arr.length ; ++j)
+        {            
+            if( arr[j] > arr[j+1])
+            {
+                let tmp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = tmp;
+                switchingFlag=true;
+            }
+        }
+        if( switchingFlag === false)
+            break;
+        else
+            switchingFlag = false
+    }
+    return arr;
+};
