ac68c033038f66ca4c77cbb9ac65fc1bd201209c
pastko <storyofstory@naver.com>
2021-07-23 09:23:00 +0900

    update
    update : codestates 코플릿 문제 테스트

--- Codestates/study-cod/js/codestates.js
+++ Codestates/study-cod/js/codestates.js
@@ -56,7 +56,7 @@ function arrayEquals(a, b) {
 }
 
 //console.log(orderOfPresentation(5, [1, 3, 2, 4, 5]));
-console.log(orderOfPresentation(3, [1, 3, 2]));
+// console.log(orderOfPresentation(3, [1, 3, 2]));
 
 
 
@@ -140,3 +140,306 @@ function flattenArr(arr) {
 
 // console.log(flattenArr( [[1], 2, [3, 4], 5]));
 
+
+
+
+
+class stack{
+    constructor(){
+        this.storage = {};
+        this.top = 0;
+    }
+
+    size() {
+        return this.top;
+    }
+
+        // 스택에 데이터를 추가 할 수 있어야 합니다.
+    push(element) {
+        this.storage[this.top] = element;
+        this.top++;
+    }
+        
+        // 가장 나중에 추가된 데이터가 가장 먼저 추출되어야 합니다.
+    pop() {
+        // 빈 스택에 pop 연산을 적용해도 에러가 발생하지 않아야 합니다
+        if (this.top === 0) {
+            return '';
+        }
+        this.top -= 1;
+        const result = this.storage[this.top];
+        delete this.storage[this.top];
+        
+        
+        return result;
+    }
+
+    get() {
+        return Object.values(this.storage);
+    }
+
+    emtpy() {
+        return Object.keys(this.storage).length === 0;
+    }
+
+    makeEmtpy() {
+        if (this.top === 0) {
+            return ;
+        }
+
+        for(let i = this.top ; i > 0 ; --i)
+        {
+            this.top -= 1;
+            const result = this.storage[this.top];
+            delete this.storage[this.top];
+        }
+        return this.top;
+    }
+}
+
+function browserStack(actions, start) {
+    // TODO: 여기에 코드를 작성합니다.
+    let prev = new stack(),
+        next = new stack();
+    let current = start;
+    actions.forEach(el => {
+        console.log(`[pre : ${el}] --   ${ prev.get()} [${ prev.size()}]  : ${current} : ${next.get()} [${ next.size()}]`)
+        if(typeof el === 'number')
+        {   
+            if(el === -1)
+            {
+                // el -1 뒤로 가기 prev
+                if( !prev.emtpy())
+                {
+                    next.push(current);
+                    current = prev.pop();
+                }
+            }
+
+            
+            if(el === 1)
+            {
+                // el 1 앞으로 가기 next
+                if( !next.emtpy() )
+                {
+                    prev.push(current);
+                    current = next.pop();
+                }
+            }
+        }else if(typeof el === 'string'){
+            prev.push(current);
+            current = el;
+
+            next.makeEmtpy();
+        }    
+        //console.log(`[post : ${el}] --   ${ prev.get()} [${ prev.size()}]  : ${current} : ${next.get()} [${ next.size()}]`)
+    });  
+    
+    return [prev.get(),current,next.get()];
+}
+
+// let actions = ["B", "C", -1, "D", "A", -1, 1, -1, -1];
+// let start = "A";
+
+// let output = browserStack(actions, start);
+// console.log(output);
+
+// let output = browserStack([-1,-1], "A");
+// console.log(output);
+
+
+class Queue {
+    constructor() {
+        this.storage = {};
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    size() {
+        return this.rear - this.front;
+    } 
+      // 큐에 데이터를 추가 할 수 있어야 합니다.
+    enqueue(element) {
+        this.storage[this.rear] = element;
+        this.rear += 1;
+    }  
+
+    get() {
+        return Object.values(this.storage);
+    }
+
+    emtpy() {
+        return Object.keys(this.storage).length === 0;
+    }
+    
+      // 가장 먼저 추가된 데이터가 가장 먼저 추출되어야 합니다.
+    dequeue() {
+      // 빈 큐에 dequeue 연산을 적용해도 에러가 발생하지 않아야 합니다
+        if (this.front === this.rear) {
+            return;
+        }
+    
+        const result = this.storage[this.front];
+        delete this.storage[this.front];
+        this.front += 1;
+    
+        return result;
+    }
+
+    makeEmtpy(){
+        this.storage = {};
+        this.rear = 0;
+        this.front = 0;
+    }
+}
+
+
+function paveBox(boxes) {
+    // TODO: 여기에 코드를 작성합니다.
+    let boxeQueue = new Queue();
+    let current   = 0;
+    let maxRes    = 0;
+
+    boxes.forEach(e=>{
+        if(boxeQueue.emtpy())
+        {
+            boxeQueue.enqueue(e);
+            current = e;
+        }
+        else{
+            if(current >= e){
+                boxeQueue.enqueue(e);
+            }
+            else{
+                let res  = boxeQueue.get().length;
+                maxRes = maxRes > res ? maxRes : res
+                boxeQueue.makeEmtpy();
+                boxeQueue.enqueue(e);current = e;
+
+            }
+        }
+        console.log(`[post : ${e}] --    ${current} : ${boxeQueue.get()} [${ boxeQueue.size()}]`)
+    })
+
+    if(!boxeQueue.emtpy())
+    {
+        let res  = boxeQueue.get().length;
+        maxRes = maxRes > res ? maxRes : res
+        boxeQueue.makeEmtpy();
+    }
+    return maxRes;
+}
+
+
+// let boxes = [5, 1, 4, 6];
+// let output = paveBox(boxes);
+// console.log(output); // 3
+
+// boxes = [95, 90, 99, 99, 80, 99];
+
+// output = paveBox(boxes);
+// console.log(output); // 1
+
+
+
+class workQueue {
+    constructor() {
+        this.storage = {};
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    working ()
+    {
+        let deleteWorkor = []
+        for(let [key, ob] of Object.entries(this.storage))
+        {
+            console.log(key,ob)
+            this.storage[key].value = ob.value--;
+            if(this.storage[key].value <= 0){
+                deleteWorkor(key);
+                this.front--;
+            }
+                
+        }
+        console.log(deleteWorkor)
+        return deleteWorkor.reduce((acc,res)=>{
+            this.enqueue(res);
+            return acc + res
+        },0)
+        
+    }
+
+    size() {
+        return this.rear - this.front;
+    } 
+      // 큐에 데이터를 추가 할 수 있어야 합니다.
+    enqueue(element) {
+        this.storage[this.rear] = element;
+        this.rear += 1;
+    }  
+
+    get() {
+        return Object.values(this.storage);
+    }
+
+    emtpy() {
+        return Object.keys(this.storage).length === 0;
+    }
+    
+      // 가장 먼저 추가된 데이터가 가장 먼저 추출되어야 합니다.
+    dequeue() {
+      // 빈 큐에 dequeue 연산을 적용해도 에러가 발생하지 않아야 합니다
+        if (this.front === this.rear) {
+            return;
+        }
+    
+        const result = this.storage[this.front];
+        delete this.storage[this.front];
+        this.front += 1;
+    
+        return result;
+    }
+
+    makeEmtpy(){
+        this.storage    = {};
+        this.rear       = 0;
+        this.front      = 0;
+    }
+}
+
+function queuePrinter(bufferSize, capacities, documents) {
+    // TODO: 여기에 코드를 작성합니다.
+    let workerQueue = new workQueue(),
+        workTime    = 0
+        workcap     = capacities;
+
+    workcap -= documents[0];
+    workerQueue.enqueue({ "key" : documents.shift() , "value" : bufferSize })
+    while(!workerQueue.emtpy() && documents.length !== 0)
+    {
+        workTime++;
+        workcap += workerQueue.working();
+
+        if(documents.length !== 0){
+            if( capacities >= workcap + documents[0])
+            {
+                workcap -= documents[0];
+                workerQueue.enqueue({ "key" : documents.shift() , "value" : bufferSize })
+            }
+        }
+        // timer ++
+        // working
+        // cap 
+        // if cap .entpy than enque
+        console.log(`[post : ${workTime}] --  : ${workerQueue.get()} [${ workerQueue.size()}] : [${ documents}]`)
+    }
+    return workTime;
+}
+
+let bufferSize = 2;
+let capacities = 10;
+let documents = [7, 4, 5, 6];
+
+let output = queuePrinter(bufferSize, capacities, documents);
+console.log(output) // 8
