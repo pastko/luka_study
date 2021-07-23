68787c6fc78c3d81e8faee7d4a4bc4c91da04dd3
pastko <storyofstory@naver.com>
2021-07-23 09:33:18 +0900

    update
    update : merge - mac

--- Codestates/study-cod/js/codestates.js
+++ Codestates/study-cod/js/codestates.js
@@ -71,6 +71,33 @@ function fibonacci(num) {
 }
 // console.log(fibonacci(9));
 
+function fibonacci(n) {
+    // TODO: 여기에 코드를 작성합니다.
+    // 재귀 사용방법 
+    // if (num <= 1) {
+    //   return num;
+    // }
+    // return fibonacci(num - 1) + fibonacci(num - 2);
+  
+  
+  
+    let results = [0, 1];  
+    let fibo = function(n) {
+      if(results[n] !== undefined){
+        return results[n] 
+      }
+      else { 
+        // 피보나치를 계산하여 계산한 결과값을 저장해 둔다.
+        results[n] = fibo(n - 2) + fibo(n - 1);
+        return results[n]
+      }
+    }
+  
+    return fibo(n)   
+  }
+  
+
+
 
 
 
