e50a078f4d4e44dd9ffb3b1e3af9a65e9b24424a
pastko <storyofstory@naver.com>
2021-07-30 17:03:02 +0900

    update
    update : toy 9 - done

--- Codestates/study-cod/js/codestates/codestates.js
+++ Codestates/study-cod/js/codestates/codestates.js
@@ -353,7 +353,7 @@ Node.prototype.addChild = function (child) {
 
 // toy 8
 
-const largestProductOfThree = function (arr) {
+const largestProductOfThree1 = function (arr) {
     // TODO: 여기에 코드를 작성합니다.
     let minus = arr.filter(e=>e<0).sort((a,b)=>b-a);
     let plus = arr.filter(e=>e>=0).sort((a,b)=>b-a);
@@ -389,6 +389,52 @@ const largestProductOfThree = function (arr) {
 
 // toy 9 
 
+function power2(base, exponent) {
+    // todo: 여기에 코드를 작성합니다.
+    let result =1.00;
+    for(let i = 0 ; i < exponent; ++i)
+    {
+        result = base *  (result);
+        console.log(i,result);
+    }
+    return result  % 94906249
+}
+function power(base, exponent) {
+    // TODO: 여기에 코드를 작성합니다.
+
+    let results = [3];  
+    let pow = function(n,count) {
+        if(count === 1){
+            return n 
+        }
+        else { 
+        // 피보나치를 계산하여 계산한 결과값을 저장해 둔다.
+            results[count] = (n * pow(n, count-1)) % 94906249;
+            console.log(results[count],count)
+            return results[count]
+        }
+    }
+    return pow(base,exponent)   
+}
+  
+function power1(base, exponent) {
+    if (exponent === 0) return 1;
+  
+    const half = parseInt(exponent / 2);
+    const temp = power(base, half);
+    const result = (temp * temp) % 94906249;
+  
+    if (exponent % 2 === 1) return (base * result) % 94906249;
+    else return result;
+}
+
+
+
+let output = power(3, 40);
+console.log(output); // --> 19334827
+
+
+
 
 
 function unpackGiftbox(giftBox, wish) {
@@ -405,8 +451,6 @@ function unpackGiftbox(giftBox, wish) {
     }
     else return unpackGiftbox(giftBox.slice(1),wish)
 }
-
-
 
 function unpackGiftbox1(giftBox, wish) {
     // TODO: 여기에 코드를 작성합니다.
