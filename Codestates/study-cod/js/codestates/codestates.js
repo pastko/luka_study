1121e23f5c60055bf823acb45f8c9f2668c4c290
pastko <storyofstory@naver.com>
2021-08-06 10:05:04 +0900

    update
    update : toy 14 -done

--- Codestates/study-cod/js/codestates/codestates.js
+++ Codestates/study-cod/js/codestates/codestates.js
@@ -596,25 +596,25 @@ let root = new Node(1);
 // leaf1.addChild(new Node(6));
 // rootChild2.addChild(new Node(7));
 
-root.addChild(new Node(2));
-root.addChild(new Node(3));
-root.addChild(new Node(4));
-root.children[0].addChild(new Node(5));
-root.children[0].addChild(new Node(6));
-root.children[0].children[0].addChild(new Node(7));
-root.children[0].children[1].addChild(new Node(8));
-root.children[1].addChild(new Node(9));
-root.children[1].addChild(new Node(10));
-root.children[1].children[1].addChild(new Node(11));
-root.children[1].children[1].addChild(new Node(12));
-root.children[2].addChild(new Node(13));
+// root.addChild(new Node(2));
+// root.addChild(new Node(3));
+// root.addChild(new Node(4));
+// root.children[0].addChild(new Node(5));
+// root.children[0].addChild(new Node(6));
+// root.children[0].children[0].addChild(new Node(7));
+// root.children[0].children[1].addChild(new Node(8));
+// root.children[1].addChild(new Node(9));
+// root.children[1].addChild(new Node(10));
+// root.children[1].children[1].addChild(new Node(11));
+// root.children[1].children[1].addChild(new Node(12));
+// root.children[2].addChild(new Node(13));
 
-output = bfs(root);
-console.log(output); // --> [1, 2, 3, 4, 5, 7, 6]
-let a = [root].shift()
-console.dir(a);
-a = [root].shift()
-console.dir(a);
+// output = bfs(root);
+// console.log(output); // --> [1, 2, 3, 4, 5, 7, 6]
+// let a = [root].shift()
+// console.dir(a);
+// a = [root].shift()
+// console.dir(a);
 
 // toy 13 
 const insertionSort = function (arr, calFlag) {
@@ -663,6 +663,52 @@ const insertionSort = function (arr, calFlag) {
 };
 
 
+
+// toy 14
+const rotatedArraySearch = function (rotated, target) {
+    // TODO : 여기에 코드를 작성합니다.
+    // let newObj = rotated.reduce((newob, e, idx)=>{
+    // newob[e] = idx
+    // return newob;
+    // },{});
+
+    // console.log(newObj);
+    // return (newObj[target]) ? newObj[target] : -1;
+    let result = -1;
+    if(rotated[0] > target)
+    {
+        for(let i = rotated.length - 1 ; i >= 0 ; --i )
+        {
+            if(rotated[i]===target){
+                result = i;
+                break;
+            }
+        }
+    }
+     // 반대로 서칭
+
+    else if(rotated[0] < target){
+        rotated.forEach((e,idx)=>{
+            if(e===target){
+                console.log(e,target,idx);
+                result = idx;
+                return;
+            }
+        });
+    }
+    //정상 서칭
+
+    return result;
+};
+
+let output = rotatedArraySearch([10, 11, 12, 3, 6, 7, 8, 9], 11);
+console.log(output); // --> 5
+
+output = rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 100);
+console.log(output); // --> -1
+
+output = rotatedArraySearch([9, 10, 15, 4, 6, 8], 6);
+console.log(output); // --> 4
 
 
 function unpackGiftbox(giftBox, wish) {
