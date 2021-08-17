194ce5d9d757391e18a89e4ddc5f85d03f5dde5b
pastko <storyofstory@naver.com>
2021-08-17 10:25:20 +0900

    update
    update : toy 20 - done

--- Codestates/study-cod/js/codestates/codestates.js
+++ Codestates/study-cod/js/codestates/codestates.js
@@ -880,19 +880,22 @@ const LPSs = function (str) {
 const mergeSort = function (arr) {
     // TODO: 여기에 코드를 작성합니다.
     if(arr.length <= 1 ) return arr;
-  
-    
-    let merge = (left, right) =>{
-        console.log(left +":" + right);
-        let result = [];
-        while (left.length !== 0 && right.length !== 0) {
-		    left[0] <= right[0] ? result.push(left.shift()) : result.push(right.shift());	
-	    }
-        return [...result, ...left, ...right];
-    }
+ 
     let centerPoint = Math.floor(arr.length / 2)
     return merge(mergeSort(arr.slice(0,centerPoint)), mergeSort(arr.slice(centerPoint)));
 };
+let merge = (left, right) =>{
+    //console.log(left +":" + right);
+    let result = [];
+    while (left.length !== 0 && right.length !== 0) {
+        left[0] <= right[0] ? result.push(left.shift()) : result.push(right.shift());	
+    }
+    
+    return [...result, ...left, ...right];
+}
+
+
+// con
   
 
 let output = mergeSort([3, 1, 21]);
