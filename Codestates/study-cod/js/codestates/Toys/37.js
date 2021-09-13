1c878cffa87d79f75699a8506c402961b319676a
pastko <storyofstory@naver.com>
2021-09-13 09:48:29 +0900

    Create 37.js
    create : toy 37 same 16_[DP] 금고를 털어라 - done

---
+++ Codestates/study-cod/js/codestates/Toys/37.js
@@ -0,0 +1,29 @@
+// toy 37_coinChange
+// HACK : 
+
+const coinChange = function (total, coins) {
+    // TODO: 여기에 코드를 작성합니다.
+    let result = 0;
+    let dp = Array(total + 1).fill(0);
+    dp[0] = 1;
+    coins.forEach(element => {
+        for (let i = element; i < total + 1; ++i) {
+            dp[i] += dp[i - element];
+            // console.table(dp);
+        }
+    });
+    return dp[total];
+};
+
+
+
+
+let total = 10;
+let coins = [1, 5];
+let output = coinChange(total, coins);
+console.log(output); // --> 3
+
+total = 4;
+coins = [1, 2, 3];
+output = coinChange(total, coins);
+console.log(output); // --> 4 ([1, 1, 1, 1], [1, 1, 2], [2, 2], [1, 3])
